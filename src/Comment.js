0dc595d7a3e54112ea3bc6f5d9df1e3583d3c1ba
997843911@qq.com <997843911@qq.com>
2019-11-17 17:00:06 +0800

    PropTypes组件参数类型限制

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types'
 
 class Comment extends Component {
+    //验证props的类型,必须为object
+    static propTypes={
+        comment:PropTypes.object.isRequired //isRequired是强制这个组件必须传入这个参数，在没设置defaultProps的情况下
+    }
 
     render() {
         return (
