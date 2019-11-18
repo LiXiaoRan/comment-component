d8612673627a16230d3dc55ca5962c63530cf626
997843911@qq.com <997843911@qq.com>
2019-11-18 09:04:43 +0800

    永久化存储评论

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -10,6 +10,23 @@ class CommentApp extends Component{
         }
     }
     
+    componentWillMount(){
+       this._loadComments();
+    }
+
+    _loadComments(){
+        let comments=localStorage.getItem('comments')
+        if (comments) {
+            comments=JSON.parse(comments);
+            this.setState({comments:comments});
+        }
+    }
+
+    _saveComments(comments){
+        //存储pinglunshuju 
+        localStorage.setItem('comments',JSON.stringify(comments));
+        console.log(JSON.stringify(comments));
+    }
 
     handleSubmitComment(comment,e){
         // 获取并更新评论数据
@@ -22,8 +39,10 @@ class CommentApp extends Component{
         if (!comment.content) {
             return alert("请输入评论内容");
         }
-        this.setState((state) => { return state.comments.push(comment) })
-        
+        let comments=this.state.comments;//这样是为了防止setState异步操作会导致少存储最新的变化
+        comments.push(comment);
+        this.setState({comments})
+        this._saveComments(comments);
     }
 
     render() {
