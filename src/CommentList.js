141df731b94bff8c3cc9de492f405eff5de6cde8
997843911@qq.com <997843911@qq.com>
2019-11-18 11:34:22 +0800

    增加了代码高亮功能

--- src/CommentList.js
+++ src/CommentList.js
@@ -20,13 +20,7 @@ class CommentList extends Component {
   }
 
   render() {
-    // let comments = [
-    //   { username: "Jerry", content: "Hello" },
-    //   { username: "Tomy", content: "World" },
-    //   { username: "Lucy", content: "Good" }
-    // ];
-
-
+    
     return <div>
         {this.props.comments.map((comment,i)=>{return <Comment comment={comment} key={i} index={i} onDeleteComment={this.handleDeleteComment.bind(this)}/>})}
     </div>;
